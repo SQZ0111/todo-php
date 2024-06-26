01cc7e0207b58fc5eb761c5d466c853219c9692b
julius-dev <juliush.dev@gmail.com>
2024-06-26 18:37:24 +0200

    changed todos rendering to view only

--- src/parts/todoCard-controller.php
+++
@@ -1,3 +0,0 @@
-  <template x-for="todo in $store.todos.todosData">  
-    <p x-text="todo.title" class='h-10 w-40 mb-2 bg-light shadow-lg bg-white-rounded'></p>; 
-  </template> 
--- src/parts/todoCard-view.php
+++ src/parts/todoCard-view.php
@@ -1,2 +1,3 @@
-<?php require __DIR__ . "/todoCard-controller.php";
-
+  <template x-for="todo in $store.todos.todosData">
+      <p x-text="todo.title" class='h-10 w-40 mb-2 bg-light shadow-lg bg-white-rounded'></p>;
+  </template>
